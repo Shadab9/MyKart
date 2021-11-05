5b0b3a0bcb1ceaf9836947ec3793204c8a4b5e7a
shadab <shadabathar.cse@gmail.com>
2021-11-06 03:25:25 +0530

    till shop cart

--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,7 @@
 # Create your views here.
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from .models import Product,Contact
+from .models import Product,Contact,Orders
 from math import ceil
 
 # Create your views here.
@@ -40,4 +40,18 @@ def prodView(request,myid):
     return render(request,'shop/prodview.html',{'product':product[0]})
 
 def checkout(request):
-    return HttpResponse("Checkout")
+    if request.method=="POST":
+        items_json= request.POST.get('itemsJson', '')
+        name=request.POST.get('name', '')
+        email=request.POST.get('email', '')
+        address=request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city=request.POST.get('city', '')
+        state=request.POST.get('state', '')
+        zip_code=request.POST.get('zip_code', '')
+        phone=request.POST.get('phone', '')
+        order = Orders(items_json= items_json, name=name, email=email, address= address, city=city, state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank=True
+        id=order.order_id
+        return render(request, 'shop/checkout.html', {'thank':thank, 'id':id})
+    return render(request, 'shop/checkout.html')
